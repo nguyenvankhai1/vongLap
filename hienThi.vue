beb51b992583565910aa9de0d3cb47595e749901
nguyenvankhai1 <59248489+nguyenvankhai1@users.noreply.github.com>
2020-01-03 09:22:05 +0700

    hienThi

---
+++ hienThi.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="app">
+           <h1>{{value}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'app',
+    data() {
+         return{
+              value : 'Hiển thị thẻ H1',
+         }
+    },
+    }
+
+</script>
+<style>
+
+</style>
